529bfa771ef80e875668a28c033e3f2c722a8cbe
Anton Kozyriev <sonara4mus@gmail.com>
2021-02-03 14:31:53 +0200

    feat(web): fake photo detection route template

--- settings.py
+++ settings.py
@@ -1,9 +1,12 @@
 import os
 
 
-PRODUCTION_MODE = os.getenv('PRODUCTION_MODE') == 'True'
+# Web application settings
+PRODUCTION_MODE = os.getenv('ENV_PRODUCTION_MODE') == 'True'
+APPLICATION_SECRET = os.getenv('ENV_APPLICATION_SECRET')
+APPLICATION_URL = os.getenv('ENV_APPLICATION_URL')
+PROTOCOL_SECRET = os.getenv('ENV_PROTOCOL_SECRET')
 
-APPLICATION_SECRET = os.getenv('APPLICATION_SECRET')
-APPLICATION_URL = os.getenv('APPLICATION_URL')
-
-PROTOCOL_SECRET = os.getenv('PROTOCOL_SECRET')
+# Deep learning classifier settings
+RESIZE_RATIO = int(os.getenv('ENV_RESIZE_RATIO'))
+KERNEL_FILE_PATH = os.getenv('ENV_KERNEL_FILE_PATH')
--- web.py
+++ web.py
@@ -1,16 +1,28 @@
-from flask import Flask
+from flask import Flask, request, make_response
 from settings import *
 
 
 app = Flask(__name__)
-app.secret_key = APPLICATION_SECRET
+app.config['SECRET_KEY'] = APPLICATION_SECRET
 
 
 @app.route('/', methods=['GET'])
 def index():
-    return "DEEP FAKE DETECTOR IS ALIVE"
+    return 'DEEP FAKE DETECTOR IS ALIVE'
+
+
+@app.route('/api/detection', methods=['POST'])
+def fake_photo_detection():
+    if 'PROTOCOL_SECRET' not in request.headers or request.headers['PROTOCOL_SECRET'] != PROTOCOL_SECRET:
+        return make_response("Protocol secret is missing or incorrect"), 401
+    elif 'photo' not in request.files:
+        return make_response("Profile photo is missing in the request body"), 400
+    else:
+        # TODO: Add fake detection functionality below
+        return make_response("Request is valid"), 200
 
 
 if __name__ == '__main__':
     app.debug = not PRODUCTION_MODE
+    app.logger.info('Application started in %s mode', "production" if PRODUCTION_MODE else "development")
     app.run()
